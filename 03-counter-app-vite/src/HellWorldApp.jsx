240bab45de543ef5e37c7e96357edbe5bc1ce0f3
Westly Meza <westlymeza@outlook.com>
2023-08-30 10:39:34 -0600

    Primer componente

---
+++ 03-counter-app-vite/src/HellWorldApp.jsx
@@ -0,0 +1,7 @@
+export const HellWorldApp = () => {
+  return (
+    <h1>HelloWorld</h1>
+  )
+}
+// shorcut para crear componente funcional
+// rafc
--- 03-counter-app-vite/src/main.jsx
+++ 03-counter-app-vite/src/main.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client'
-
-
-function App(){
-    return <h1>Hola mundo</h1>
-}
+import { HellWorldApp } from './HellWorldApp';
+import { FirstApp } from './FirstApp';
 
 ReactDOM.createRoot(document.getElementById('root'))
 .render(
     <React.StrictMode>
-        <App />
+        <HellWorldApp />
+        <FirstApp />
     </React.StrictMode>
 );
