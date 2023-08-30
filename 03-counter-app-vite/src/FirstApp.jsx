76a5f96073e7c5dbe6e8586ac6182c8be860cc59
Westly Meza <westlymeza@outlook.com>
2023-08-30 13:31:41 -0600

    mostrando variables desde js

--- 03-counter-app-vite/src/FirstApp.jsx
+++ 03-counter-app-vite/src/FirstApp.jsx
@@ -1,6 +1,17 @@
 
+const getMessage = ()=>{
+    return {
+        message: 'This is a Message',
+        title : 'Hello World'
+    };
+}
+
 export const FirstApp = () => {
   return (
-    <div>FirstApp</div>
+    <>
+        <div>FirstApp</div>
+        <code>{JSON.stringify(getMessage())}</code>
+    </>
+    
   )
 }
