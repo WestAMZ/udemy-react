020547fb6fb86614110de111ef2a37d44f554ef9
Westly Meza <westlymeza@gmail.com>
2023-10-23 23:39:22 -0600

    MultipleCustomHooks component working

--- 05-hook-app/src/03-examples/MultipleCustomHooks.jsx
+++ 05-hook-app/src/03-examples/MultipleCustomHooks.jsx
@@ -1,13 +1,42 @@
-import { useFetch } from "../hooks/useFetch"
-import { useCounter } from "../hooks/useCounter";
+import { useFetch, useCounter } from "../hooks/";
+
+
+const LoadingComponent = ()=> {
+  return (
+    <div className="alert alert-info text-center">
+      Loading...
+    </div>
+  );
+} 
+
+const CardItem = (data)=>{
+  const { forms, sprites, types } =  data;
+  console.log({ forms, sprites, types });
+  return (
+    <div className="card bg-light" style={{width: '18rem'}}>
+      <img src={ sprites?.front_default } alt="pokemon sprite" className="card-img-top bg-white"/>
+      <div className="card-body">
+        <h5 className="card-title">{ forms[0]?.name }</h5>
+        <div className="d-flex p-2">
+          {
+            types.map((item,index)=>{
+              console.log({item,index})
+              return (
+                <span className={`badge me-2 bg-${item.type.name} p-2`} key={`type-${index}`}>{ item.type.name }</span>
+              )
+            })
+          }
+        </div>
+      </div>
+    </div>
+  );
+}
 
 export const MultipleCustomHooks = () => {
 
   const { counter, increment, decrement } = useCounter(1,1);
   const {data, isLoading, hasError } = useFetch(`https://pokeapi.co/api/v2/pokemon/${counter}`);
-
-
-  const { forms, sprites, types } = !!data && data;
+  // const { forms, sprites, types } = !!data && data;
 
   return (
     <>
@@ -15,38 +44,13 @@ export const MultipleCustomHooks = () => {
         <hr/>
 
       {
-        isLoading 
-        ?
-          (
-            <div className="alert alert-info text-center">
-              Loading...
-            </div>
-          )
-        :
-          (
-            <div className="card bg-light" style={{width: '18rem'}}>
-              <img src={ sprites.front_default } alt="pokemon sprite" className="card-img-top bg-white"/>
-              <div className="card-body">
-                <h5 className="card-title">{ forms[0]?.name }</h5>
-                <div className="d-flex p-2">
-                  {
-                    types.map((item,index)=>{
-                      console.log({item,index})
-                      return (
-                        <span className={`badge me-2 bg-${item.type.name} p-2`} key={`type-${index}`}>{ item.type.name }</span>
-                      )
-                    })
-                  }
-                </div>
-              </div>
-            </div>
-          )
+        isLoading  ? <LoadingComponent /> : <CardItem {...data} />
       }
 
-      <button className="btn btn-outline-primary mt-2" onClick={()=>{ decrement(1) }}>
+      <button className="btn btn-outline-primary mt-2" onClick={()=>{ decrement(1) }} disabled={isLoading}>
         Previous Pokemon
       </button>
-      <button className="btn btn-outline-primary mt-2" onClick={()=>{ increment(1) }}>
+      <button className="btn btn-outline-primary mt-2" onClick={()=>{ increment(1) }} disabled={isLoading}>
         Next Pokemon
       </button>
 
